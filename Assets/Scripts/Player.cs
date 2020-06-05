744ff1325547efb196e23be9fcdfcfeb4203d8ac
Reetro <thegamerboy7@gmail.com>
2020-06-05 14:18:52 -0600

    Player can now jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,20 +8,25 @@ public class Player : MonoBehaviour
     [Header("Player Movement")]
     [Tooltip("Multiplier for player run speed")]
     [SerializeField] float runSpeed = 5f;
+    [Tooltip("Amount to add to player's Y velocity when jumping")]
+    [SerializeField] float jumpSpeed = 5f;
 
     Rigidbody2D myRigidBody = null;
     Animator myAnimator = null;
+    Collider2D myCollider = null;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
@@ -32,18 +37,32 @@ public class Player : MonoBehaviour
         
         myRigidBody.velocity = playerVelocity;
 
-        bool playerIsMoving = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", IsPlayerMovingHorizontaly());
+    }
 
-        myAnimator.SetBool("Running", playerIsMoving);
+    private void Jump()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                Vector2 jumpVelcityToAdd = new Vector2(0.0f, jumpSpeed);
+
+                myRigidBody.velocity += jumpVelcityToAdd;
+            }
+        }
     }
 
     private void FlipSprite()
     {
-        bool playerHasHorizontalMovement = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-
-        if (playerHasHorizontalMovement)
+        if (IsPlayerMovingHorizontaly())
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }
+    }
+
+    private bool IsPlayerMovingHorizontaly()
+    {
+        return Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
     }
 }
