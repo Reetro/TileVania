07842f052220f308785a086e08cd3f91b56946b9
Reetro <thegamerboy7@gmail.com>
2020-06-09 15:20:51 -0600

    created moving water

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+
+    int startingSceneIndex = 0;
+
+    private void Awake()
+    {
+        int scenePersistNum = FindObjectsOfType<ScenePersist>().Length;
+
+        if (scenePersistNum > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;  
+    }
+
+    void Update()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
