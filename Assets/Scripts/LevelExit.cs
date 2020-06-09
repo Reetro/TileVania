b09d4b9034a11dff36c958a31c399ec88660c720
Reetro <thegamerboy7@gmail.com>
2020-06-09 12:38:39 -0600

    Can now load into new levels

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSecondsRealtime(loadDelay);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        bool goBackToStart = currentSceneIndex >= SceneManager.sceneCountInBuildSettings;
+
+        if (!goBackToStart)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
